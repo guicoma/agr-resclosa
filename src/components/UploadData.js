6b581ac11c95117d72d1ea7e2409e89d25c6056a
Coma <guillemcoma@gmail.com>
2018-11-14 09:11:38 +0100

    parsefloat console removed

--- src/components/UploadData.js
+++ src/components/UploadData.js
@@ -19,7 +19,6 @@ function parseFile(file){
   values = values.map((item)=>{
     let aux   = item.split("\t");
     let floataux = aux[1].replace(",",".");
-    console.log('asdsfg', parseFloat(floataux), floataux);
     let obj = {
       datetime: aux[0].replace(new RegExp("/", 'g'),"-"),
       value   : parseFloat(floataux)
@@ -87,7 +86,6 @@ class UploadData extends Component {
           let reader = new FileReader();
           // Closure to capture the file information.
           reader.onload = (function() {
-            console.log("file.name",file.name)
             return function(e) {
               let parsed = parseFile(e.currentTarget.result);
               if(file.name.includes("VA")){

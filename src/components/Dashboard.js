f12561311cd2780834c7fc0dac5956c8fe85433b
Coma <guillemcoma@gmail.com>
2018-11-15 03:00:44 +0100

    Fix data retreval

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BarChart, Bar, LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import moment from "moment"
 import { withRouter } from 'react-router-dom';
-import { Table, Button, Divider, Icon } from 'antd';
+import { Table, Button, Divider, Icon, message } from 'antd';
 import { merge } from 'lodash';
 
 import './../styles/Dashboard.css';
@@ -14,101 +14,130 @@ const columns = [{
   dataIndex: 'datetime',
   key: 'datetime',
 }, {
-  title: 'Cabal mitjà horari',
+  title: 'Cabal mitjà horari (L/s)',
   dataIndex: 'avg_flow',
   key: 'avg_flow',
 }, {
-  title: 'Volume accumulated',
+  title: 'Volume accumulated (h^3)',
   dataIndex: 'volume_acc',
   key: 'volume_acc',
 }];
 
+function handleErrors(response) {
+  if (!response.ok) {
+      throw Error(response.statusText);
+  }
+  return response.json();
+}
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
       volume_data: [],
       flow_data: [],
       table_data: [],
       width: 700,
       height: 500,
-      year: moment().year(),
-      month: moment().month()
+      year: 2018,
+      month: 10
     }
   }
 
-  componentDidMount() {
-
-    /*
-    let data = [];
-    let data2 = [];
-    
-    for (let x = 1; x <= 720; x++) {
-      data.push({datetime: x, avg_flow: Math.floor(Math.random() * 100)})
-      data2.push({datetime: x, volume_acc: Math.floor(Math.random() * 100)})
-    }
-
-    let tdata = merge(data,data2);
-
-    this.setState({
-      flow_data: data,
-      volume_data: data2,
-      table_data: tdata
-    });
-
-    console.log("DATA");
-    console.log(tdata);
-    */
-
-    let flowData, volumeData, tableData;
-
-    fetch('./api/flow/read.php')
-      .then((r) => r.json())
-      .then((r) => {
-        flowData = r.records.map((item) => {
-          return {
-            datetime: moment(item.datetime).format("YYYY-MM-DD HH"),
-            avg_flow: parseFloat(item.avg_flow)
-          }
-        });
-        console.log('read-flow',flowData);
-        return fetch('./api/volume/read.php');
+  fetchData(year, month) {
+    let flowData = [], volumeData = [], tableData = [], dateRange;
+  
+      dateRange = {
+        year: year,
+        month: month
+      }
+      //fetch('./api/flow/read.php')
+      fetch('./api/flow/readRange.php', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dateRange)
       })
-      .then((r) => r.json())
-      .then((r) => {
-        volumeData = r.records.map((item) => {
-          return {
-            datetime  : moment(item.datetime).format("YYYY-MM-DD HH"),
-            volume_acc: parseFloat(item.volume_acc)
-          }
+      .then(handleErrors)
+      .then((flow) => {
+        flowData = flow.records;
+        //console.log('read-flow',flowData);
+        this.setState({flow_data: flowData});
+        //return fetch('./api/volume/read.php');
+        return fetch('./api/volume/readRange.php', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(dateRange)
         });
-        
-        tableData = merge(flowData, volumeData);
+      })
+      .then(handleErrors)
+      .then((volume) => {
+        volumeData = volume.records;
 
+        let auxFlow = flowData.slice();
+        let auxVol  = volumeData.slice();
+        
+        tableData = merge(auxVol, auxFlow).reverse();
+  /*
         console.log('DATA');
         console.log('flowData', flowData);
         console.log('volumeData', volumeData);
-        console.log('tableData', tableData);
-
+        console.log('tableData', tableData.filter((item) => { return ("avg_flow" in item)}));
+  */
         this.setState({
-          flow_data   : flowData,
+          loading     : false,
+          year        : year,
+          month       : month,
           volume_data : volumeData,
-          table_data  : tableData
+          table_data  : tableData.filter((item) => { return ("avg_flow" in item)})
         });
-
+  
       })
       .catch((e) => {
         console.error(e);
+        message.error('Error while getting info');
+        this.setState({loading : false});
       });
   }
 
+  componentDidMount() {
+    this.fetchData(this.state.year, this.state.month);
+  }
+
+  searchPrevYear = () => {
+    this.setState({loading:true});
+    this.fetchData(this.state.year-1,this.state.month);
+  }
+
+  searchNextYear = () => {
+    this.setState({loading:true});
+    this.fetchData(this.state.year+1, this.state.month);
+  }
+
+  searchPrevMonth = () => {
+    this.setState({loading:true});
+    this.fetchData(this.state.year, this.state.month-1);
+  }
+
+  searchNextMonth = () => {
+    this.setState({loading:true});
+    this.fetchData(this.state.year, this.state.month+1);
+  }
+  
   render() {
     return (
       <div>
+        <h1>Resclosa - Moli de la Coromina</h1>
+        <h4>Registre d'Aigues: A-0012541</h4>
         <div className="dashboard">
           <div>
-            <Divider>Volumen acumulat (h^3) - {this.state.year}</Divider>
+            <Divider>Volumen acumulat (h<sup>3</sup>) - {this.state.year}</Divider>
             <div className="graph">
               <LineChart width={700} height={420} data={this.state.volume_data}>
                 <Line type="monotone" dataKey="volume_acc" stroke="#8884d8" dot={false} />
@@ -118,13 +147,13 @@ class Dashboard extends Component {
               </LineChart>
             </div>
             <ButtonGroup>
-              <Button><Icon type="left" /></Button>
-              <Button type="dashed" disabled>{this.state.year}</Button>
-              <Button disabled={this.state.year === moment().year()}><Icon type="right" /></Button>
+              <Button onClick={this.searchPrevYear} disabled={this.state.loading}><Icon type="left" /></Button>
+              <Button type="dashed" loading={this.state.loading}>{this.state.year}</Button>
+              <Button onClick={this.searchNextYear} disabled={this.state.loading || (this.state.year === moment().year())}><Icon type="right" /></Button>
             </ButtonGroup>
           </div>
           <div>
-            <Divider>Últims 30 dies de dades de Cabal mitjà horari (L/s) - {this.state.month}</Divider>
+            <Divider>Cabal mitjà horari (L/s) - {moment(this.state.month, "M").format("MMMM")}</Divider>
             <div className="graph">
               <BarChart width={700} height={420} data={this.state.flow_data}
                 margin={{top: 5, right: 30, left: 20, bottom: 5}}>
@@ -135,9 +164,9 @@ class Dashboard extends Component {
               </BarChart>
             </div>
             <ButtonGroup>
-              <Button><Icon type="left" /></Button>
-              <Button type="dashed" disabled>{this.state.month}</Button>
-              <Button disabled={this.state.month === moment().month()}><Icon type="right" /></Button>
+              <Button onClick={this.searchPrevMonth} disabled={this.state.loading}><Icon type="left" /></Button>
+              <Button type="dashed" loading={this.state.loading}>{moment(this.state.month, "M").format("MMMM")}</Button>
+              <Button onClick={this.searchNextMonth} disabled={this.state.loading || (this.state.month === moment().month() && this.state.year === moment().year())}><Icon type="right" /></Button>
             </ButtonGroup>
           </div>
         </div>
